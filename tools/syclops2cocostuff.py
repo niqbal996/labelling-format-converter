9798c639825d44dddf9e3be56c9e04f25d13ba4d
Naeem Iqbal <naeemiqbal996@gmail.com>
2024-04-19 17:38:20 +0200

    Added colorizer

--- tools/syclops2cocostuff.py
+++ tools/syclops2cocostuff.py
@@ -9,22 +9,22 @@ color_map = {
     2: [0, 0, 255], # red
 }
 # /home/niqbal/git/syclops/output/2024_04_06_14_00_06/main_camera_annotations/semantic_segmentation
-input_dir = '/mnt/e/datasets/sugarbeet_syn_v1/sugarbeet_syn_v2/main_camera_annotations/semantic_segmentation'
-# out_dir = os.path.join(input_dir, 'semantics')
-# os.makedirs(out_dir, exist_ok=True)
+input_dir = '/home/niqbal/git/syclops/output/sugarbeet_syn_v2/main_camera_annotations/semantic_segmentation'
+out_dir = os.path.join(input_dir, 'semantics')
+os.makedirs(out_dir, exist_ok=True)
 input_images = glob(input_dir + '/*.npz')
 #cv2.namedWindow('transformed image', cv2.WINDOW_NORMAL)
-# for image_path in input_images:
-#     arr = np.load(image_path)['array'].astype(np.uint8)
-#     cv2.imwrite(os.path.join(out_dir, os.path.basename(image_path[:-3]+'png')), arr)
-
-
 for image_path in input_images:
     arr = np.load(image_path)['array'].astype(np.uint8)
-    color = np.zeros((arr.shape[0], arr.shape[1], 3), dtype=np.uint8)
-    for i in range(3):
-        color[arr == i] = color_map[i]
-    cv2.imshow(os.path.basename(image_path), color)
-    cv2.waitKey(0)
+    cv2.imwrite(os.path.join(out_dir, os.path.basename(image_path[:-3]+'png')), arr)
+
+
+# for image_path in sorted(input_images):
+#     arr = np.load(image_path)['array'].astype(np.uint8)
+#     color = np.zeros((arr.shape[0], arr.shape[1], 3), dtype=np.uint8)
+#     for i in range(3):
+#         color[arr == i] = color_map[i]
+#     cv2.imwrite(os.path.join(out_dir, os.path.basename(image_path[:-3]+'png')), color)
+    # cv2.waitKey(0)
 
 
